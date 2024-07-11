076361b7a75231b00a523b2b2d689d5f0ba73dc1
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-07-11 14:34:43 -0400

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -375,7 +375,7 @@
         <div class="container-login100">
             <div class="wrap-login100">
                 <div class="login100-pic js-tilt" data-tilt>
-                    <img src="{{ asset('images/img-01.png') }}" alt="IMG">
+                    <img src="{{ url('images/MONITO.png') }}" alt="IMG">
                 </div>
                 <form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
                     @csrf
