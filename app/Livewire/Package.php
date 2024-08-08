3cf8ed55c25ab23597bd8b57cda937c5b87df322
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-08-08 09:06:34 -0400

    Update Package.php

--- app/Livewire/Package.php
+++ app/Livewire/Package.php
@@ -4,7 +4,7 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Livewire\WithPagination;
-use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
 use Illuminate\Pagination\LengthAwarePaginator;
 use App\Exports\PackagesExport;
 use Carbon\Carbon;
@@ -40,56 +40,70 @@ class Package extends Component
 
     public function getFilteredPackages()
     {
-        $response = Http::withOptions([
-            'verify' => false, // Deshabilitar la verificación del certificado SSL
+        $client = new Client([
+            'verify' => false, // Deshabilitar verificación SSL
             'curl' => [
-                CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_3, // Especificar la versión TLS
+                CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_3, // Usar TLSv1.3
             ],
-        ])->withHeaders([
-            'Authorization' => 'Bearer eZMlItx6mQMNZjxoijEvf7K3pYvGGXMvEHmQcqvtlAPOEAPgyKDVOpyF7JP0ilbK'
-        ])->get('https://correos.gob.bo:8000/api/softdeletes');
+        ]);
 
-        $packages = [];
-        if ($response->successful()) {
-            $packages = $response->json();
-            
-            foreach ($packages as &$package) {
-                if (isset($package['deleted_at'])) {
-                    $package['deleted_at'] = Carbon::parse($package['deleted_at'])->format('d-m-Y H:i:s');
+        try {
+            $response = $client->request('GET', 'https://correos.gob.bo:8000/api/softdeletes', [
+                'headers' => [
+                    'Authorization' => 'Bearer eZMlItx6mQMNZjxoijEvf7K3pYvGGXMvEHmQcqvtlAPOEAPgyKDVOpyF7JP0ilbK',
+                ]
+            ]);
+
+            $packages = [];
+            if ($response->getStatusCode() == 200) {
+                $packages = json_decode($response->getBody(), true);
+
+                foreach ($packages as &$package) {
+                    if (isset($package['deleted_at'])) {
+                        $package['deleted_at'] = Carbon::parse($package['deleted_at'])->format('d-m-Y H:i:s');
+                    }
                 }
             }
-        }
 
-        if (!empty($this->search)) {
-            $packages = array_filter($packages, function($package) {
-                return stripos($package['CODIGO'], $this->search) !== false || 
-                       stripos($package['DESTINATARIO'], $this->search) !== false;
+            // Filtrar los paquetes según la búsqueda
+            if (!empty($this->search)) {
+                $packages = array_filter($packages, function($package) {
+                    return stripos($package['CODIGO'], $this->search) !== false || 
+                           stripos($package['DESTINATARIO'], $this->search) !== false;
+                });
+            }
+
+            // Filtrar los paquetes según la fecha
+            if (!empty($this->date)) {
+                $packages = array_filter($packages, function($package) {
+                    return Carbon::parse($package['deleted_at'])->toDateString() == Carbon::parse($this->date)->toDateString();
+                });
+            }
+
+            // Filtrar los paquetes según la ventanilla
+            if (!empty($this->ventanilla)) {
+                $packages = array_filter($packages, function($package) {
+                    return $package['VENTANILLA'] === $this->ventanilla;
+                });
+            }
+
+            // Filtrar los paquetes según la ciudad
+            if (!empty($this->ciudad)) {
+                $packages = array_filter($packages, function($package) {
+                    return stripos($package['CUIDAD'], $this->ciudad) !== false;
+                });
+            }
+
+            usort($packages, function ($a, $b) {
+                return Carbon::parse($b['deleted_at']) <=> Carbon::parse($a['deleted_at']);
             });
+
+            return $packages;
+
+        } catch (\Exception $e) {
+            // Manejar el error
+            return [];
         }
-
-        if (!empty($this->date)) {
-            $packages = array_filter($packages, function($package) {
-                return Carbon::parse($package['deleted_at'])->toDateString() == Carbon::parse($this->date)->toDateString();
-            });
-        }
-
-        if (!empty($this->ventanilla)) {
-            $packages = array_filter($packages, function($package) {
-                return $package['VENTANILLA'] === $this->ventanilla;
-            });
-        }
-
-        if (!empty($this->ciudad)) {
-            $packages = array_filter($packages, function($package) {
-                return stripos($package['CUIDAD'], $this->ciudad) !== false;
-            });
-        }
-
-        usort($packages, function ($a, $b) {
-            return Carbon::parse($b['deleted_at']) <=> Carbon::parse($a['deleted_at']);
-        });
-
-        return $packages;
     }
 
     public function generatePDF()
