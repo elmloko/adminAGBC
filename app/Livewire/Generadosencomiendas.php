1e9599f695db7e2b89385b9db22eab45e444fee0
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-09-25 17:44:07 -0400

    CAMBIOS CLASIFICACION

--- app/Livewire/Generadosencomiendas.php
+++ app/Livewire/Generadosencomiendas.php
@@ -3,11 +3,131 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Pagination\LengthAwarePaginator;
+use App\Exports\PackagesExport;
+use Carbon\Carbon;
+use PDF;
+use Excel;
 
 class Generadosencomiendas extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+    public $date = '';
+    public $ventanilla = '';
+    public $ciudad = '';
+    public $filteredPackages = [];
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function searchPackages()
+    {
+        $this->resetPage();
+        $this->filteredPackages = $this->getFilteredPackages();
+    }
+
+    public function applyFilters()
+    {
+        $this->resetPage();
+        $this->filteredPackages = $this->getFilteredPackages();
+    }
+
+    public function getFilteredPackages()
+    {
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer eZMlItx6mQMNZjxoijEvf7K3pYvGGXMvEHmQcqvtlAPOEAPgyKDVOpyF7JP0ilbK'
+        ])->withOptions([ 'verify' => false,
+        ])->get('https://correos.gob.bo:8000/api/callclasiUENCOMIENDAS');
+
+        $packages = [];
+        if ($response->successful()) {
+            $packages = $response->json();
+
+            foreach ($packages as &$package) {
+                if (isset($package['created_at'])) {
+                    $package['created_at'] = Carbon::parse($package['created_at'])->format('d-m-Y H:i:s');
+                }
+            }
+        }
+
+        // Filtrar los paquetes según la búsqueda
+        if (!empty($this->search)) {
+            $packages = array_filter($packages, function ($package) {
+                return stripos($package['CODIGO'], $this->search) !== false ||
+                    stripos($package['DESTINATARIO'], $this->search) !== false;
+            });
+        }
+
+        // Filtrar los paquetes según la fecha
+        if (!empty($this->date)) {
+            $packages = array_filter($packages, function ($package) {
+                return Carbon::parse($package['created_at'])->toDateString() == Carbon::parse($this->date)->toDateString();
+            });
+        }
+
+        // Filtrar los paquetes según la ventanilla
+        if (!empty($this->ventanilla)) {
+            $packages = array_filter($packages, function ($package) {
+                return $package['VENTANILLA'] === $this->ventanilla;
+            });
+        }
+
+        // Filtrar los paquetes según la ciudad
+        if (!empty($this->ciudad)) {
+            $packages = array_filter($packages, function ($package) {
+                return stripos($package['CUIDAD'], $this->ciudad) !== false;
+            });
+        }
+
+        // Filtrar los paquetes cuyo estado sea "DESPACHO" o "CLASIFICACION"
+        $packages = array_filter($packages, function ($package) {
+            return $package['ESTADO'] === 'DESPACHO' || $package['ESTADO'] === 'CLASIFICACION';
+        });
+
+        usort($packages, function ($a, $b) {
+            return Carbon::parse($b['created_at']) <=> Carbon::parse($a['created_at']);
+        });
+
+        return $packages;
+    }
+
+    public function generatePDF()
+    {
+        $packages = $this->getFilteredPackages();
+        $pdf = PDF::loadView('livewire.package-pdf', compact('packages'));
+        return response()->streamDownload(function () use ($pdf) {
+            echo $pdf->stream();
+        }, 'correspondencia_entregada.pdf');
+    }
+
+    public function generateExcel()
+    {
+        $packages = $this->getFilteredPackages();
+        return Excel::download(new PackagesExport($packages), 'Reporte Paquetes Ordinarios.xlsx');
+    }
+
     public function render()
     {
-        return view('livewire.generadosencomiendas');
+        $packages = $this->filteredPackages;
+        if (empty($packages)) {
+            $packages = $this->getFilteredPackages();
+        }
+
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 10;
+        $currentItems = array_slice($packages, ($currentPage - 1) * $perPage, $perPage);
+        $paginatedItems = new LengthAwarePaginator($currentItems, count($packages), $perPage, $currentPage, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        return view('livewire.generadosencomiendas', [
+            'packages' => $paginatedItems
+        ]);
     }
 }
--- resources/views/livewire/generadoseca.blade.php
+++ resources/views/livewire/generadoseca.blade.php
@@ -1,3 +1,95 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
+                        <span id="card_title">
+                            {{ __('CORRESPONDENCIA AREA DE CLASIFICACION') }}
+                        </span>
+                    </div>
+                    <div style="display: flex; align-items: center;">
+                        <input type="text" wire:model="search" placeholder="Buscar por código o destinatario..."
+                            class="form-control mr-2">
+                        <button wire:click="searchPackages" class="btn btn-primary mr-2">Buscar</button>
+                        <input type="date" wire:model="date" class="form-control mr-2">
+                        <select wire:model="ciudad" class="form-control mr-2">
+                            <option value="">Todas las Ciudades</option>
+                            <option value="LA PAZ">LA PAZ</option>
+                            <option value="COCHABAMBA">COCHABAMBA</option>
+                            <option value="SANTA CRUZ">SANTA CRUZ</option>
+                            <option value="ORURO">ORURO</option>
+                            <option value="POTOSI">POTOSI</option>
+                            <option value="SUCRE">SUCRE</option>
+                            <option value="BENI">BENI</option>
+                            <option value="PANDO">PANDO</option>
+                            <option value="TARIJA">TARIJA</option>
+                        </select>
+                        <select wire:model="ventanilla" class="form-control mr-2">
+                            <option value="">Todas las Ventanillas</option>
+                            <option value="DD">DD</option>
+                            <option value="DND">DND</option>
+                            <option value="ENCOMIENDAS">ENCOMIENDAS</option>
+                            <option value="ECA">ECA</option>
+                            <option value="UNICA">UNICA</option>
+                            <option value="CASILLAS">CASILLAS</option>
+                        </select>
+                        <select wire:model="estado" class="form-control mr-2">
+                            <option value="">Todos las estados</option>
+                            <option value="CLASIFICACION">CLASIFICACION</option>
+                            <option value="DESPACHO">DESPACHO</option>
+                        </select>
+                        <button wire:click="applyFilters" class="btn btn-secondary">Aplicar Filtros</button>
+                        <button wire:click="generatePDF" class="btn btn-danger">Generar PDF</button>
+                        <button wire:click="generateExcel" class="btn btn-success">Generar Excel</button>
+                    </div>
+                </div>
+            </div>
+            <div wire:loading>
+                <div class="overlay">
+                    <i class="fas fa-2x fa-sync fa-spin justify-content-center"></i>
+                </div>
+            </div>
+            @if (session()->has('success'))
+                <div class="alert alert-success">
+                    <p>{{ session('success') }}</p>
+                </div>
+            @endif
+            <div class="card-body">
+                <div class="table-responsive">
+                    <div class="d-flex justify-content-center">
+                        Se encontraron {{ $packages->total() }} registros en total
+                    </div>
+                    <table class="table table-striped table-hover">
+                        <thead class="thead">
+                            <tr>
+                                <th>CODIGO</th>
+                                <th>DESTINATARIO</th>
+                                <th>PESO</th>
+                                <th>ESTADO</th>
+                                <th>PROXIMO ESTADO</th>
+                                <th>CREADO EN</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($packages as $package)
+                                <tr>
+                                    <td>{{ $package['CODIGO'] }}</td>
+                                    <td>{{ $package['DESTINATARIO'] }}</td>
+                                    <td>{{ $package['PESO'] }}</td>
+                                    <td>{{ $package['ESTADO'] }}</td>
+                                    <td>VENTANILLA / CARTEROS</td>
+                                    <td>{{ $package['created_at'] }}</td>
+                                    <td></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            {{ $packages->links('pagination::bootstrap-4') }}
+        </div>
+    </div>
 </div>
