e5b477a54b5dd6389480be2e982e72f17e042a8a
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-09-26 12:39:06 -0400

    Cambios Ventanilla

--- app/Livewire/Ventanillaencomiendas.php
+++ app/Livewire/Ventanillaencomiendas.php
@@ -3,11 +3,157 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Pagination\LengthAwarePaginator;
+use App\Exports\PackagesExport;
+use Carbon\Carbon;
+use PDF;
+use Excel;
 
 class Ventanillaencomiendas extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+    public $date = '';
+    public $ventanilla = '';
+    public $ciudad = '';
+    public $filteredPackages = [];
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function searchPackages()
+    {
+        $this->resetPage();
+        $this->filteredPackages = $this->getFilteredPackages();
+    }
+
+    public function applyFilters()
+    {
+        $this->resetPage();
+        $this->filteredPackages = $this->getFilteredPackages();
+    }
+
+    public function getFilteredPackages()
+    {
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer eZMlItx6mQMNZjxoijEvf7K3pYvGGXMvEHmQcqvtlAPOEAPgyKDVOpyF7JP0ilbK'
+        ])->withOptions([
+            'verify' => false,
+        ])->get('https://correos.gob.bo:8000/api/packagesUENCOMIENDAS');
+    
+        $packages = [];
+        if ($response->successful()) {
+            $packages = $response->json();
+    
+            foreach ($packages as &$package) {
+                // Formatear la fecha de actualización
+                if (isset($package['updated_at'])) {
+                    $package['updated_at'] = Carbon::parse($package['updated_at'])->format('d-m-Y H:i:s');
+                }
+    
+                // Calcular la diferencia de días entre created_at y updated_at
+                if (isset($package['created_at'], $package['updated_at'])) {
+                    $createdAt = Carbon::parse($package['created_at']);
+                    $updatedAt = Carbon::parse($package['updated_at']);
+                    $package['date_difference'] = $updatedAt->diffInDays($createdAt);
+                } else {
+                    $package['date_difference'] = 'No actualizado'; // O cualquier mensaje que desees mostrar
+                }
+    
+                // Determinar el estado basado en date_difference
+                if ($package['date_difference'] <= 7) {
+                    $package['next_status'] = 'VENTANILLA';
+                    $package['status_color'] = 'green';
+                } elseif ($package['date_difference'] > 7 && $package['date_difference'] <= 30) {
+                    $package['next_status'] = 'CARTERO';
+                    $package['status_color'] = 'yellow';
+                } else {
+                    $package['next_status'] = 'REZAGO';
+                    $package['status_color'] = 'red';
+                }
+            }
+        }
+    
+        // Filtrar por estado
+        if (!empty($this->ventanilla)) {
+            $packages = array_filter($packages, function ($package) {
+                return $package['next_status'] === $this->ventanilla; // Filtrado por next_status
+            });
+        }
+    
+        // Filtrar los paquetes según la búsqueda
+        if (!empty($this->search)) {
+            $packages = array_filter($packages, function ($package) {
+                return stripos($package['CODIGO'], $this->search) !== false ||
+                    stripos($package['DESTINATARIO'], $this->search) !== false;
+            });
+        }
+    
+        // Filtrar los paquetes según la fecha
+        if (!empty($this->date)) {
+            $packages = array_filter($packages, function ($package) {
+                return Carbon::parse($package['created_at'])->toDateString() == Carbon::parse($this->date)->toDateString();
+            });
+        }
+    
+        // Filtrar los paquetes según la ciudad
+        if (!empty($this->ciudad)) {
+            $packages = array_filter($packages, function ($package) {
+                return stripos($package['CUIDAD'], $this->ciudad) !== false;
+            });
+        }
+    
+        // Ordenar los paquetes
+        usort($packages, function ($a, $b) {
+            if (is_null($a['updated_at']) && is_null($b['updated_at'])) {
+                return 0;
+            } elseif (is_null($a['updated_at'])) {
+                return 1;
+            } elseif (is_null($b['updated_at'])) {
+                return -1;
+            }
+            return Carbon::parse($b['updated_at']) <=> Carbon::parse($a['updated_at']);
+        });
+    
+        return $packages;
+    }
+    
+    public function generatePDF()
+    {
+        $packages = $this->getFilteredPackages();
+        $pdf = PDF::loadView('livewire.package-pdf', compact('packages'));
+        return response()->streamDownload(function () use ($pdf) {
+            echo $pdf->stream();
+        }, 'correspondencia_entregada.pdf');
+    }
+
+    public function generateExcel()
+    {
+        $packages = $this->getFilteredPackages();
+        return Excel::download(new PackagesExport($packages), 'Reporte Paquetes Ordinarios.xlsx');
+    }
+
     public function render()
     {
-        return view('livewire.ventanillaencomiendas');
+        $packages = $this->filteredPackages;
+        if (empty($packages)) {
+            $packages = $this->getFilteredPackages();
+        }
+
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 10;
+        $currentItems = array_slice($packages, ($currentPage - 1) * $perPage, $perPage);
+        $paginatedItems = new LengthAwarePaginator($currentItems, count($packages), $perPage, $currentPage, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        return view('livewire.ventanillaencomiendas', [
+            'packages' => $paginatedItems
+        ]);
     }
 }
--- resources/views/livewire/ventanillaencomiendas.blade.php
+++ resources/views/livewire/ventanillaencomiendas.blade.php
@@ -1,3 +1,81 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
+                        <span id="card_title">
+                            {{ __('CORRESPONDENCIA AREA DE VENTANILLA') }}
+                        </span>
+                    </div>
+                    <div style="display: flex; align-items: center;">
+                        <input type="text" wire:model="search" placeholder="Buscar por código o destinatario..."
+                            class="form-control mr-2">
+                        <button wire:click="searchPackages" class="btn btn-primary mr-2">Buscar</button>
+                        <input type="date" wire:model="date" class="form-control mr-2">
+                        <select wire:model="ventanilla" class="form-control mr-2">
+                            <option value="">Seleccionar Estado</option>
+                            <option value="VENTANILLA">VENTANILLA</option>
+                            <option value="CARTERO">CARTERO</option>
+                            <option value="REZAGO">REZAGO</option>
+                        </select>
+                        <button wire:click="applyFilters" class="btn btn-secondary">Aplicar Filtros</button>
+                        <button wire:click="generatePDF" class="btn btn-danger">Generar PDF</button>
+                        <button wire:click="generateExcel" class="btn btn-success">Generar Excel</button>
+                    </div>
+                </div>
+            </div>
+            <div wire:loading>
+                <div class="overlay">
+                    <i class="fas fa-2x fa-sync fa-spin justify-content-center"></i>
+                </div>
+            </div>
+            @if (session()->has('success'))
+                <div class="alert alert-success">
+                    <p>{{ session('success') }}</p>
+                </div>
+            @endif
+            <div class="card-body">
+                <div class="table-responsive">
+                    <div class="d-flex justify-content-center">
+                        Se encontraron {{ $packages->total() }} registros en total
+                    </div>
+                    <table class="table table-striped table-hover">
+                        <thead class="thead">
+                            <tr>
+                                <th>CODIGO</th>
+                                <th>DESTINATARIO</th>
+                                <th>PESO</th>
+                                <th>PROXIMO ESTADO</th>
+                                <th>RECIBIDO</th>
+                                <th>TIEMPO TRASCURRIDO</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($packages as $package)
+                                <tr>
+                                    <td>{{ $package['CODIGO'] }}</td>
+                                    <td>{{ $package['DESTINATARIO'] }}</td>
+                                    <td>{{ $package['PESO'] }}</td>
+                                    <td>
+                                        <span
+                                            style="color: {{ $package['status_color'] }}">{{ $package['next_status'] }}</span>
+                                    </td>
+                                    <td>{{ $package['updated_at'] }}</td>
+                                    <td>
+                                        <span
+                                            style="color: {{ $package['status_color'] }}">{{ $package['date_difference'] }}
+                                            días</span>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            {{ $packages->links() }}
+        </div>
+    </div>
 </div>
