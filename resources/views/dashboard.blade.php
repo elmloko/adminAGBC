098579bae65da8f0c32a476107468c853f6d8961
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-09-27 12:47:11 -0400

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,11 +3,10 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Sistema de Informacion y Reclamos de la AGBC</h1>
+    <h1>Sistema Administrativo de la AGBC</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
     @include('footer')
 @stop
 
