df03c6baf09af3f8a4eb494d66b523ae9f7bcff4
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-09-30 09:32:54 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
