815a3e83c17b007b3e0bd90dd66179a88f48f8ad
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-02-05 17:02:39 -0400

    Cambios Despachos 1

--- app/Livewire/Apertura.php
+++ app/Livewire/Apertura.php
@@ -3,11 +3,86 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class Apertura extends Component
 {
+    public $aperturas = [];
+    public $search = '';
+    public $date = '';
+    public $categoria = '';
+
+    public function mount()
+    {
+        $this->fetchData();
+    }
+
+    public function fetchData()
+    {
+        $response = Http::withOptions([
+            'verify' => false, // Ignorar la verificación SSL
+        ])->get('https://correos.gob.bo:8005/api/apertura');
+        
+        if ($response->successful()) {
+            $data = $response->json();
+            if ($data['success'] && isset($data['data'])) {
+                $this->aperturas = array_map(function ($item) {
+                    return [
+                        'oforigen' => $item['oforigen'],
+                        'ofdestino' => $item['ofdestino'],
+                        'identificador' => $item['identificador'],
+                        'created_at' => date('Y-m-d H:i:s', strtotime($item['created_at'])),
+                        'peso_total' => array_sum(array_column($item['sacas'], 'peso')),
+                        'paquetes_total' => array_reduce($item['sacas'], function ($carry, $saca) {
+                            return $carry + array_reduce($saca['contenidos'], function ($subcarry, $contenido) {
+                                return $subcarry + 
+                                    ($contenido['nropaquetesro'] ?? 0) + 
+                                    ($contenido['nropaquetesbl'] ?? 0) + 
+                                    ($contenido['nropaquetesof'] ?? 0) + 
+                                    ($contenido['nropaquetesii'] ?? 0) + 
+                                    ($contenido['nropaqueteset'] ?? 0) + 
+                                    ($contenido['nropaquetessu'] ?? 0) + 
+                                    ($contenido['nropaquetessn'] ?? 0) + 
+                                    ($contenido['nropaquetesems'] ?? 0) + 
+                                    ($contenido['nropaquetescp'] ?? 0) + 
+                                    ($contenido['nropaquetesco'] ?? 0) + 
+                                    ($contenido['sacasm'] ?? 0) + 
+                                    ($contenido['lcao'] ?? 0);
+                            }, 0);
+                        }, 0),
+                    ];
+                }, $data['data']);
+            }
+        }
+    }
+
+    public function applyFilters()
+    {
+        $this->fetchData();
+        
+        if ($this->search) {
+            $this->aperturas = array_filter($this->aperturas, function ($apertura) {
+                return stripos($apertura['identificador'], $this->search) !== false;
+            });
+        }
+        
+        if ($this->date) {
+            $this->aperturas = array_filter($this->aperturas, function ($apertura) {
+                return $apertura['created_at'] === $this->date;
+            });
+        }
+        
+        if ($this->categoria) {
+            $this->aperturas = array_filter($this->aperturas, function ($apertura) {
+                return $apertura['oforigen'] === $this->categoria;
+            });
+        }
+    }
+
     public function render()
     {
-        return view('livewire.apertura');
+        return view('livewire.apertura', [
+            'aperturas' => $this->aperturas
+        ]);
     }
 }
--- resources/views/livewire/apertura.blade.php
+++ resources/views/livewire/apertura.blade.php
@@ -1,3 +1,71 @@
-<div>
-    {{-- Be like water. --}}
-</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
+                        <h4 id="card_title">
+                            {{ __('Despachos Aperturados') }}
+                        </h4>
+                    </div>
+                </div>
+                <div style="display: flex; align-items: center;">
+                    <input type="text" wire:model="search" placeholder="Buscar por nombre..."
+                        class="form-control mr-2">
+                    <input type="date" wire:model="date" class="form-control mr-2">
+                    <select wire:model="categoria" class="form-control mr-2">
+                        <option value="">Todos las regionales</option>
+                        <option value="BOLPZ">BOLPZ - LA PAZ</option>
+                        <option value="BOTJA">BOTJA - TARIJA</option>
+                        <option value="BOPOI">BOPOI - POTOSI</option>
+                        <option value="BOCBB">BOCBB - COCHABAMBA</option>
+                        <option value="BOCIJ">BOCIJ - PANDO</option>
+                        <option value="BOORU">BOORU - ORURO</option>
+                        <option value="BOTDD">BOTDD - BENI</option>
+                        <option value="BOSRE">BOSRE - SUCRE</option>
+                        <option value="BOSRZ">BOSRZ - SANTA CRUZ</option>
+                    </select>
+                    <button wire:click="applyFilters" class="btn btn-primary mr-2">Buscar</button>
+                </div>
+                @if (session()->has('success'))
+                    <div class="alert alert-success">
+                        <p>{{ session('success') }}</p>
+                    </div>
+                @endif
+                <div class="card-body">
+                    <div class="table-responsive">
+                        {{-- <div class="d-flex justify-content-center">
+                        Se encontraron {{ $aperturas->total() }} registros en total
+                    </div> --}}
+                        <table class="table table-striped table-hover">
+                            <thead class="thead">
+                                <tr>
+                                    <th>Despacho</th>
+                                    <th>Oficina Origen</th>
+                                    <th>Oficina Destino</th>
+                                    <th>Peso Total</th>
+                                    <th>Paquetes Totales</th>
+                                    <th>Fecha Apertura</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($aperturas as $apertura)
+                                    <tr>
+                                        <td>{{ $apertura['identificador'] }}</td>
+                                        <td>{{ $apertura['oforigen'] }}</td>
+                                        <td>{{ $apertura['ofdestino'] }}</td>
+                                        <td>{{ $apertura['peso_total'] }} kg</td>
+                                        <td>{{ $apertura['paquetes_total'] }}</td>
+                                        <td>{{ $apertura['created_at'] }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            {{-- <div class="d-flex justify-content-center">
+            {{ $aperturas->links('pagination::bootstrap-4') }}
+        </div> --}}
+        </div>
+    </div>
