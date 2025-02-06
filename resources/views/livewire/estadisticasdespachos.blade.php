9556f6898376414c79abcfe283b61e78c5d48461
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-02-06 14:35:35 -0400

    Update estadisticasdespachos.blade.php

--- resources/views/livewire/estadisticasdespachos.blade.php
+++ resources/views/livewire/estadisticasdespachos.blade.php
@@ -1,6 +1,6 @@
 <div class="card card-dark">
     <div class="card-header">
-        <h3 class="card-title">Estadísticas del Sistema Área Clasificación</h3>
+        <h3 class="card-title">Estadísticas del Sistema de Gestion de Sacas</h3>
         <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse">
                 <i class="fas fa-minus"></i>
