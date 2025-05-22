96e7b3e83e54469f6e1ce9b66e3403cfb7afebda
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-05-22 16:51:47 -0400

    Revert "QA 1"
    This reverts commit 5307c515454e4ab9ac94a40e72ed1b60496d1bf1.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,9 +6,6 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Pagination\Paginator;
 use App\Models\User; // Importa la clase User correcta
-use Illuminate\Support\Facades\Cookie;
-use Symfony\Component\HttpFoundation\Cookie as SymfonyCookie;
-use Illuminate\Support\Facades\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,26 +22,6 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
         Gate::define('viewPulse', function (?User $user) {
             return $user !== null;
-        });
-        // 🔒 Agrega HttpOnly a XSRF-TOKEN (opcional, si NO lo usas con JavaScript)
-        app('router')->middleware('web')->group(function () {
-            app('events')->listen('Illuminate\Routing\Events\RouteMatched', function () {
-                $token = csrf_token();
-    
-                Cookie::queue(
-                    new SymfonyCookie(
-                        'XSRF-TOKEN',
-                        $token,
-                        now()->addMinutes(config('session.lifetime')),
-                        '/',
-                        null,
-                        config('session.secure'),
-                        true, // HttpOnly
-                        false,
-                        'Lax'
-                    )
-                );
-            });
         });
     }
 }
