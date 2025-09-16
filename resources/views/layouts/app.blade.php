6f56901d0b5a83efa3301024bf93dfdedf9eb985
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-09-15 21:25:31 -0400

    Google

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,18 +39,6 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-KG6059QG44"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-KG6059QG44');
-    </script>
 </body>
 
 </html>
